0a86aaa7853db4fdd90c3bfbe6a4f2febbd89b7c
taxi-alicante <serviciotaxienalicante@gmail.com>
2026-07-30 18:05:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,12 +5,12 @@ const urlsToCache = [
   '/index.html',
   '/en.html',
   '/site.webmanifest',
-  '/contacto/android-chrome-192x192.png',
-  '/contacto/android-chrome-512x512.png',
-  '/contacto/apple-touch-icon.png',
-  '/contacto/favicon.ico',
-  '/contacto/favicon-32x32.png',
-  '/contacto/favicon-16x16.png'
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/favicon.ico',
+  '/favicon-32x32.png',
+  '/favicon-16x16.png'
 ];
 
 // INSTALACIÓN
